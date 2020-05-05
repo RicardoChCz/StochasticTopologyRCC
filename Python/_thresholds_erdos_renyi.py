87e68094f1f33a740d4675b7a8dea557a030d83b
Ricardo Chavez <ricardo.chavez@weenjoy.com>
2020-05-05 18:20:05 -0500

    ER thresholds

--- Python/_thresholds_erdos_renyi.py
+++ Python/_thresholds_erdos_renyi.py
@@ -8,43 +8,73 @@ Proability thresholds for Erdös-Renyi model
 """
 
 import matplotlib.pyplot as plt
-from _aux import intervalo
+from __aux import intervalo
 from math import log
-import numpy as np
 
 """
-1. Functions to determine whether or not a vertex v is uniquely determinated by
-a random subset of vertices of the graph in G(n,p).
+1. Lower bundles for properties in ER model
 """
 
-def probability_diameter(n,c):
+def bundle_connectivity(n):
     """
-    Returns the probability (aproximated) that in a G(n,p) i.e. ER-model a 
-    random set of size k uniqely determines the nth-vertex.
-    Input:(int, float, int)
-    Output: float
+    Returns the lower bundle for diameter treshold
+    Input: n
+    Output: p(n)
     """
+    return log(n) / n
 
-    return ((n*log(n**2/c))**(1/2.0)) / n
+def bundle_diameter(n):
+    """
+    Returns the lower bundle for diameter treshold
+    Input: n
+    Output: p(n)
+    """
+    return 1.0 / n**(0.5)
 
+def bundle_locally_infinite(n):
+    """
+    Returns the lower bundle for locally infinite treshold
+    Input: n
+    Output: p(n)
+    """
+    return 1.0 / n
 
 def graph_curves():
     """
-    Print graphs with aproximated probabilities and its coutes
-    Input: list N, list P, int k
+    Print bundles
+    Input: -
     Output: -
     """
-    I = intervalo(2,1000,10)
+    I = intervalo(5,100,1)
     curve = [0]*(len(I))
-    lineColor="red"
-    for c in [x/100.0 for x in range(1,10)]:
-        print(c)
-        for i in range(0, len(I)):
-            curve[i] = probability_diameter(I[i],c)
-            plt.plot(I, curve,linewidth=1,linestyle='-',color=lineColor,label="c= 0.5")
-
-    plt.show()
+    properties = [
+      {
+        'bundle': bundle_locally_infinite,
+        'color': '#ebc844',
+        'label': 'Locally infinite'
+      },
+      {
+        'bundle': bundle_connectivity,
+        'color': '#f16c20',
+        'label': 'Connected'
+      },
+      {
+         'bundle': bundle_diameter,
+         'color': '#0d3c55',
+         'label': 'Diameter 2'
+      }
+    ]
+    for prop in properties:
+        for i in range(0,I.size):
+            curve[i]=prop['bundle'](I[i])
+            
+        plt.plot(I, curve,linewidth=1.5,linestyle='-',
+                color=prop['color'],label=prop['label'])
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', 
+                 borderaxespad=0.)
+        plt.fill_between(I, curve, 0.5, facecolor=prop['color'], alpha=0.4)
     
-
+    plt.savefig('Figures/ER-thresholds.png')
+    plt.show()
 if __name__ == "__main__":
     graph_curves()
